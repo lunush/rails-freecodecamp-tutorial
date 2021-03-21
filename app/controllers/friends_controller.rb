26f19870de12d3aa6c7c1dc2adacb0884878a376
Lunush <76045303+lunush@users.noreply.github.com>
2021-03-21 12:35:50 +0300

    Associate two tables with each other

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -64,6 +64,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :birthday, :phone, :email, :likes_icecream)
+      params.require(:friend).permit(:first_name, :last_name, :birthday, :phone, :email, :likes_icecream, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_115634) do
+ActiveRecord::Schema.define(version: 2021_03_21_091406) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_03_19_115634) do
     t.boolean "likes_icecream"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
